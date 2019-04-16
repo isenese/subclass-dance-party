0f4c062ae920950cf4d5ef019571d604593d300f
jvc5546 <jvc5546@gmail.com>
2019-04-16 18:50:17 -0500

    Removes unnecesary properties from dancer.animated css selector rules, and adds infinite bouncing when you hover on gif. Need to prevent infinitely bounding

--- src/init.js
+++ src/init.js
@@ -6,33 +6,30 @@ $(document).ready(function() {
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
     var dancerMakerFunction = window[dancerMakerFunctionName];
     // make a dancer with a random position
+    if(window.dancers.length === 30){
+      alert('The Fire Code doesn\'t allow more than 30 dancers on the dancefloor');
+    } else {
+      var max = 0.80;
+      var min = 0.60;
 
-    var max = 0.75;
-    var min = 0.55;
-
-    var dancer = new dancerMakerFunction(
-      $("body").height() * (Math.random() * (max - min) + min),
-      $("body").width() * Math.random(),
-      Math.random() * 1000
-    );
-    $('.dancers_container').append(dancer.$node);
-    window.dancers.push(dancer);
+      var dancer = new dancerMakerFunction(
+        $("body").height() * (Math.random() * (max - min) + min),
+        $("body").width() * Math.random(),
+        Math.random() * 1000
+      );
+      dancer.$node.hover(function(){
+        dancer.$node.addClass("animated bounce");
+      });
+      $('.dancers_container').append(dancer.$node);
+      window.dancers.push(dancer);
+    }
   });
-
-  // $('.lineupButton').on('click', function(event) {
-  //   // $('.dancer').each(function(index){
-  //   //   this.style = (" top: 70vh");
-  //   // });
-
-  //   window.dancers.forEach(function(dancer){
-  //     dancer.$node.addClass('lineup');
-  //   });
-  // });
-
   $('.lineupButton').on('click', function(event) {
     $('.dancer').each(function(index){
       this.style = ("top:0", "position: relative");
     });
+
+
   });
 
 });
