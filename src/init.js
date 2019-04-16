d6791b19b548ddb3e450f98c844c240b6820ffb2
jvc5546 <jvc5546@gmail.com>
2019-04-16 15:40:01 -0500

    Added Lisa dancer and removed BinkyDancer

--- src/init.js
+++ src/init.js
@@ -2,28 +2,20 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
     // make a dancer with a random position
+    if(dancerMakerFunctionName === 'MakeLisaDancer'){
+      var max = 0.8;
+      var min = 0.6;
+    } else{
+      var max = 1;
+      var min = 0.8;
+    }
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
+      $("body").height() * (Math.random() * (max - min) + min),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
@@ -32,18 +24,11 @@ $(document).ready(function() {
   });
 
   $('.lineupButton').on('click', function(event) {
-    window.dancers.forEach(function(dancer){
-      //dancer.$node.attr('position','relative');
-      dancer.$node.animate({top : 50},2000);
+    $('.dancer').each(function(index){
+      this.style = ("top: 80%");
     });
   });
 
-  //In Here
-
-  // $('.lineupButton').on('click', function(event) {
-  //   $('.dancer').each(function(index){
-  //     this.style = (" top: 25%") })
-  //   });
 });
 
 
---
+++ src/lisaDancer.js
@@ -0,0 +1,11 @@
+var MakeLisaDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this,top,left,timeBetweenSteps);
+  this.$node.append('<iframe src="https://giphy.com/embed/uglH7hBsJrTRm" width="200" height="160" frameBorder="0"  ></iframe><p><a href="https://giphy.com/stickers/the-simpsons-funny-uglH7hBsJrTRm"></a></p>');
+};
+
+MakeLisaDancer.prototype = Object.create(MakeDancer.prototype);
+MakeLisaDancer.prototype.constructor = MakeLisaDancer;
+// MakeBlinkyDancer.prototype.step = function () {
+//   MakeDancer.prototype.step.call(this);
+//   this.$node.toggle();
+// };
