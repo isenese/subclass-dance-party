6d6d433ebe80daf59a3744d67d95894cb2598fa2
jvc5546 <jvc5546@gmail.com>
2019-04-16 20:31:26 -0500

    Added Circle up button and dancers 'shake' when teh 30 dancer limit is reached

--- src/init.js
+++ src/init.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
     // make a dancer with a random position
     if(window.dancers.length === 30){
       alert('The Fire Code doesn\'t allow more than 30 dancers on the dancefloor');
+      window.dancers.forEach(function(dancer){
+        dancer.$node.addClass("animated shake");
+      });
     } else {
       var max = 0.80;
       var min = 0.60;
@@ -17,21 +20,46 @@ $(document).ready(function() {
         $("body").width() * Math.random(),
         Math.random() * 1000
       );
-      dancer.$node.hover(function(){
+      // dancer.$node.hover(function(){
+      //   dancer.$node.addClass("animated bounce");
+      // });
+      dancer.$node.mouseenter(function(){
         dancer.$node.addClass("animated bounce");
+      });
+      dancer.$node.mouseleave(function(){
+        dancer.$node.removeClass("animated bounce");
       });
       $('.dancers_container').append(dancer.$node);
       window.dancers.push(dancer);
     }
   });
+
+
   $('.lineupButton').on('click', function(event) {
     $('.dancer').each(function(index){
       this.style = ("top:0", "position: relative");
     });
-
-
   });
 
+  $('.circleButton').on('click',function(event){
+    type = 1;
+    radius = '12em';
+    start = -90;
+
+    numberOfElements = (type === 1) ?  window.dancers.length : window.dancers.length - 1,
+
+    slice = 360 * type / numberOfElements;
+
+    window.dancers.forEach(function(dancer,index) {
+      var $self = dancer.$node,
+        rotate = slice * index + start,
+        rotateReverse = rotate * -1;
+
+      $self.css({
+        'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+      });
+    });
+  });
 });
 
 
